a97ce725e366c80f53b1d0c9328696e4451a0248
Franz <franzmarte23@gmail.com>
2023-12-13 11:54:32 -0800

    made changes

--- app/models/apartment.rb
+++ app/models/apartment.rb
@@ -1,5 +1,5 @@
 class Apartment < ApplicationRecord
     belongs_to :user
 
-    validates :street, :unit, :city, :state, :square_footage, :price, :bedrooms, :bathrooms, :pets, :image, presence: true
+    validates :street, :unit, :city, :state, :square_footage, :price, :bedrooms, :bathrooms, :pets, :image, presence: true, length: {minimum: 1}
 end
