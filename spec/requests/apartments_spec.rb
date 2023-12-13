b07dde8af64bf3e744ea6eaff8be3a4a9a3666fa
jacob <jacobamezola@gmail.com>
2023-12-12 16:39:10 -0800

    added 422 validations

--- spec/requests/apartments_spec.rb
+++ spec/requests/apartments_spec.rb
@@ -73,25 +73,218 @@ RSpec.describe "Apartments", type: :request do
           expect(apartment.user_id).to eq user.id
         end
 
-        it "cant create an apartment" do
-            apartment_params = {
+        it "cant create an apartment without a unit" do
+        apartment_params = {
             apartment: {
-            street: "Walaby Way",
-            city: "Sydney",
-            state: "Australia",
-            square_footage: 1500,
-            price: "3000",
-            bedrooms: 3,
-            bathrooms: 2,
-            pets: "fish only",
-            image: "https://images.unsplash.com/photo-1540448051910-09cfadd5df61?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-            user_id: user.id
+                street: "Walaby Way",
+                city: "Sydney",
+                state: "Australia",
+                square_footage: 1500,
+                price: "3000",
+                bedrooms: 3,
+                bathrooms: 2,
+                pets: "fish only",
+                image: "https://images.unsplash.com/photo-1540448051910-09cfadd5df61?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+                user_id: user.id
             }
             }
+        
             post '/apartments', params: apartment_params
             apartment = JSON.parse(response.body)
             expect(response).to have_http_status(422)
             expect(apartment['unit']).to include "can't be blank"
+        end
+
+        it "cant create an apartment without a street" do
+            apartment_params = {
+                apartment: {
+                    unit:"42",
+                    city: "Sydney",
+                    state: "Australia",
+                    square_footage: 1500,
+                    price: "3000",
+                    bedrooms: 3,
+                    bathrooms: 2,
+                    pets: "fish only",
+                    image: "https://images.unsplash.com/photo-1540448051910-09cfadd5df61?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+                    user_id: user.id
+                }
+                }
+            
+            post '/apartments', params: apartment_params
+            apartment = JSON.parse(response.body)
+            expect(response).to have_http_status(422)
+            expect(apartment['street']).to include "can't be blank"
+        end
+
+        it "cant create an apartment without a city" do
+            apartment_params = {
+                apartment: {
+                    unit:"42",
+                    street: "walaby way",
+                    state: "Australia",
+                    square_footage: 1500,
+                    price: "3000",
+                    bedrooms: 3,
+                    bathrooms: 2,
+                    pets: "fish only",
+                    image: "https://images.unsplash.com/photo-1540448051910-09cfadd5df61?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+                    user_id: user.id
+                    }
+                    }
+                
+            post '/apartments', params: apartment_params
+            apartment = JSON.parse(response.body)
+            expect(response).to have_http_status(422)
+            expect(apartment['city']).to include "can't be blank"
+        end
+
+        it "cant create an apartment without a state" do
+            apartment_params = {
+                apartment: {
+                    unit:"42",
+                    street: "walaby way",
+                    city: "Sydney",
+                    square_footage: 1500,
+                    price: "3000",
+                    bedrooms: 3,
+                    bathrooms: 2,
+                    pets: "fish only",
+                    image: "https://images.unsplash.com/photo-1540448051910-09cfadd5df61?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+                    user_id: user.id
+                        }
+                        }
+                    
+            post '/apartments', params: apartment_params
+            apartment = JSON.parse(response.body)
+            expect(response).to have_http_status(422)
+            expect(apartment['state']).to include "can't be blank"
+        end
+                
+        it "cant create an apartment without a square footage" do
+            apartment_params = {
+                apartment: {
+                    unit:"42",
+                    street: "walaby way",
+                    state: "Australia",
+                    city: "sydney",
+                    price: "3000",
+                    bedrooms: 3,
+                    bathrooms: 2,
+                    pets: "fish only",
+                    image: "https://images.unsplash.com/photo-1540448051910-09cfadd5df61?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+                    user_id: user.id
+                }
+                }
+            post '/apartments', params: apartment_params
+            apartment = JSON.parse(response.body)
+            expect(response).to have_http_status(422)
+            expect(apartment['square_footage']).to include "can't be blank"
+        end
+
+        it "cant create an apartment without a price" do
+            apartment_params = {
+                apartment: {
+                    unit:"42",
+                    street: "walaby way",
+                    state: "Australia",
+                    square_footage: 1500,
+                    city: "Sydney",
+                    bedrooms: 3,
+                    bathrooms: 2,
+                    pets: "fish only",
+                    image: "https://images.unsplash.com/photo-1540448051910-09cfadd5df61?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+                    user_id: user.id
+                }
+                }
+            post '/apartments', params: apartment_params
+            apartment = JSON.parse(response.body)
+            expect(response).to have_http_status(422)
+            expect(apartment['price']).to include "can't be blank"
+        end
+                        
+        it "cant create an apartment without bedrooms" do
+            apartment_params = {
+                apartment: {
+                    unit:"42",
+                    street: "walaby way",
+                    state: "Australia",
+                    square_footage: 1500,
+                    price: "3000",
+                    city: "sydney",
+                    bathrooms: 2,
+                    pets: "fish only",
+                    image: "https://images.unsplash.com/photo-1540448051910-09cfadd5df61?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+                    user_id: user.id
+                }
+                }
+            post '/apartments', params: apartment_params
+            apartment = JSON.parse(response.body)
+            expect(response).to have_http_status(422)
+            expect(apartment['bedrooms']).to include "can't be blank"
+        end
+
+        it "cant create an apartment without bathrooms" do
+            apartment_params = {
+                apartment: {
+                    unit:"42",
+                    street: "walaby way",
+                    state: "Australia",
+                    square_footage: 1500,
+                    price: "3000",
+                    bedrooms: 3,
+                    city: "sydney",
+                    pets: "fish only",
+                    image: "https://images.unsplash.com/photo-1540448051910-09cfadd5df61?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+                    user_id: user.id
+                }
+                }
+            post '/apartments', params: apartment_params
+            apartment = JSON.parse(response.body)
+            expect(response).to have_http_status(422)
+            expect(apartment['bathrooms']).to include "can't be blank"
+        end
+
+        it "cant create an apartment without a pets" do
+            apartment_params = {
+                apartment: {
+                    unit:"42",
+                    street: "walaby way",
+                    state: "Australia",
+                    square_footage: 1500,
+                    price: "3000",
+                    bedrooms: 3,
+                    bathrooms: 2,
+                    city: "sydney",
+                    image: "https://images.unsplash.com/photo-1540448051910-09cfadd5df61?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+                    user_id: user.id
+                }
+                }
+            post '/apartments', params: apartment_params
+            apartment = JSON.parse(response.body)
+            expect(response).to have_http_status(422)
+            expect(apartment['pets']).to include "can't be blank"
+        end
+
+        it "cant create an apartment without an image" do
+            apartment_params = {
+                apartment: {
+                    unit:"42",
+                    street: "walaby way",
+                    state: "Australia",
+                    square_footage: 1500,
+                    price: "3000",
+                    bedrooms: 3,
+                    bathrooms: 2,
+                    pets: "fish only",
+                    city: "sydney",
+                    user_id: user.id
+                }
+                }
+            post '/apartments', params: apartment_params
+            apartment = JSON.parse(response.body)
+            expect(response).to have_http_status(422)
+            expect(apartment['image']).to include "can't be blank"
         end
     end
 end
